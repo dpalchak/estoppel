dde986550233a5d82c465fa64d4870e140583bf6
David R. Palchak <david@palchak.net>
2018-07-11 18:18:35 -0700

    Start lib/hw/cortex_m4f library
    * Add basepri register handling functions

---
+++ lib/hw/cortex_m4f/basepri.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "base/macros.h"
+#include <cstdint>
+
+namespace estp {
+
+// Get the current BASEPRI value
+uint32_t INLINE LoadBasePriority() {
+  uint32_t result;
+  asm volatile ("MRS %0, basepri" : "=r" (result) );
+  return(result);
+}
+
+// Unconditionally set the BASEPRI value
+void INLINE StoreBasePriority(uint32_t value) {
+  asm volatile ("MSR basepri, %0" : : "r" (value) : "memory");
+}
+
+// Set the BASEPRI_MAX value
+// Only interrupts with a priority value below the BASEPPRI_MAX will run
+// When writing to BASEPRI_MAX, the BASEPRI register is only affected if either:
+// - The new priority is non-zero and the current BASEPRI value is 0
+// - The new priority is non-zero and is smaller (higher priority) than the current BASEPRI value.
+void INLINE StoreMaxBasePriority(uint32_t value) {
+  asm volatile ("MSR basepri_max, %0" : : "r" (value) : "memory");
+}
+
+} // namespace
