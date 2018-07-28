011d73d9fcb211be293d56f47b920f734c62be28
David R. Palchak <david@palchak.net>
2018-07-27 18:45:56 -0700

    Add initial base/types.h
    * Includes StringLiteral<N>, ZString, and CZString typedefs

---
+++ lib/base/types.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+namespace estp {
+
+using size_t = std::size_t;
+
+// A convenience typedef for readability
+template<std::size_t N>
+using StringLiteral = char const (&)[N];
+
+// Readability types for 'zero' (aka null) terminated strings
+// These are borrowed from the C++ Guideline Support Library (GSL)
+using ZString = char *;
+using CZString = char const *;
+
+}
