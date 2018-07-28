23fa9d476b2ee5ec344ff5856881b7117e4a0ae5
David R. Palchak <david@palchak.net>
2018-07-27 18:47:18 -0700

    Add MetaString data type
    The MetaString<...> class is a meta-type for compile time string
    manipulation. MetaStrings are most easily created using the
    MAKE_METASTRING_XX(_s) macros, where XX stipulates the maximum length
    of the string literal argument. Valid values for XX are 16, 64, and
    256.
    
    The following operations are provided for MetaStrings:
    Comparison (equality), Get (element), Left, Right, Reverse, Substring,
    LStrip, Rstrip, Strip, Split, Join, Concatenate

---
+++ lib/data/meta_string.h
@@ -0,0 +1,386 @@
+#pragma once
+
+#include "base/meta.h"
+#include "base/types.h"
+#include "data/meta_list.h"
+
+#include <tuple>
+
+namespace estp {
+
+template<char... Chars>
+struct MetaString  {
+    constexpr static std::size_t length {sizeof...(Chars)};
+
+    // Include an implicit null terminator for compatibility with normal string-handling functions
+    constexpr static char const value[sizeof...(Chars)+1] { Chars..., '\0'};
+
+    constexpr static CZString cbegin() {
+    	return &value[0];
+    }
+
+    constexpr static CZString cend() {
+    	return &value[length];
+    }
+
+    constexpr operator StringLiteral<length+1>() const {
+    	return value;
+    }
+};
+
+template<char... C>
+constexpr char const MetaString<C...>::value[sizeof...(C)+1];
+
+template<>
+struct MetaString<> {
+    constexpr static std::size_t length {0};
+
+    // Include an implicit null terminator for compatibility with normal string-handling functions
+    constexpr static char const value[] {'\0'};
+
+    constexpr static CZString cbegin() {
+    	return &value[0];
+    }
+
+    constexpr static CZString cend() {
+    	return &value[length];
+    }
+
+    constexpr operator StringLiteral<length+1>() const {
+    	return value;
+    }
+};
+
+constexpr char const MetaString<>::value[1];
+
+using NullMetaString = MetaString<>;
+
+template<typename First, typename Second>
+struct MetaStringPair {
+	constexpr static First const first{};
+	constexpr static Second const second{};
+};
+
+
+namespace _metastring {
+
+struct StringLiteralTooLong {};
+
+// Return the element at the specified index,
+// or null if the index is out of range
+template<size_t N, size_t M>
+constexpr char GetCharOrNull(StringLiteral<M> str) {
+	return (N < M) ? str[N] : '\0';
+}
+
+// Used to check MetaString length
+template<bool B, char... Chars>
+constexpr auto CheckMetaStringMaxLen(MetaString<Chars...>) {
+	if constexpr(Bool<B, MetaString<Chars...>>::value) {
+		return MetaString<Chars...>{};
+	} else {
+		static_assert(Bool<B, MetaString<Chars...>>::value,
+			"Length of string literal exceeds METASTRING_MAX_LEN");
+		return StringLiteralTooLong{};
+	}
+}
+
+template<size_t N, char First, char... Rest>
+constexpr char _Get(MetaString<First>, MetaString<Rest>...) {
+	if constexpr(0 == N) {
+		return First;
+	} else {
+		return _Get<N-1>(MetaString<Rest>{}...);
+	}
+}
+
+template<size_t N, char... Head>
+constexpr auto _First(MetaString<Head...>, NullMetaString) {
+	static_assert(sizeof...(Head) <= N);
+	return MetaString<Head...>{};
+}
+
+template<size_t N, char... Head, char Next, char... Rest>
+constexpr auto _First(MetaString<Head...>, MetaString<Next, Rest...>) {
+	if constexpr(sizeof...(Head) < N) {
+		return _First<N>(MetaString<Head..., Next>{}, MetaString<Rest...>{});
+	} else {
+		return MetaString<Head...>{};
+	}
+}
+
+template<size_t N, char... Head>
+constexpr auto _Last(MetaString<Head...>, NullMetaString) {
+	return NullMetaString{};
+}
+
+template<size_t N, char... Head, char Next, char... Rest>
+constexpr auto _Last(MetaString<Head...>, MetaString<Next, Rest...>) {
+	if constexpr(1+sizeof...(Rest) > N) {
+		return _Last<N>(MetaString<Head..., Next>{}, MetaString<Rest...>{});
+	} else {
+		return MetaString<Next,Rest...>{};
+	}
+}
+
+template<char... Rev>
+constexpr auto _Reverse(NullMetaString, MetaString<Rev...>) {
+	return MetaString<Rev...>{};
+}
+
+template<char First, char... Fwd, char... Rev>
+constexpr auto _Reverse(MetaString<First, Fwd...>, MetaString<Rev...>) {
+	return _Reverse(MetaString<Fwd...>{}, MetaString<First, Rev...>{});
+}
+
+constexpr auto _LStrip(NullMetaString) {
+	return NullMetaString{};
+}
+
+template<char First, char... Rest>
+constexpr auto _LStrip(MetaString<First, Rest...>) {
+	if constexpr((' ' == First) || ('\t' == First)) {
+		return _LStrip(MetaString<Rest...>{});
+	} else {
+		return MetaString<First, Rest...>{};
+	}
+}
+
+template<char X, char... Buf, typename... Pieces>
+constexpr auto _Split(NullMetaString, MetaString<Buf...>, MetaList<Pieces...>) {
+	return Append(MetaList<Pieces...>{}, MetaString<Buf...>{});
+}
+
+template<char X, char First, char... Rest, char... Buf, typename... Pieces>
+constexpr auto _Split(MetaString<First, Rest...>, MetaString<Buf...>, MetaList<Pieces...>) {
+	if constexpr(First == X) {
+		return _Split<X>(MetaString<Rest...>{}, NullMetaString{},
+			Append(MetaList<Pieces...>{}, MetaString<Buf...>{}));
+	} else {
+		return _Split<X>(MetaString<Rest...>{}, MetaString<Buf..., First>{}, MetaList<Pieces...>{});
+	}
+}
+
+} // namespace _metastring
+
+template<size_t N, char... Chars>
+constexpr char Get(MetaString<Chars...>) {
+	static_assert(N < sizeof...(Chars), "Index out of range");
+	return _metastring::_Get<N>(MetaString<Chars>{}...);
+}
+
+template<size_t N, char... Chars>
+constexpr char RGet(MetaString<Chars...>) {
+	static_assert(N < sizeof...(Chars), "Index out of range");
+	return _metastring::_Get<sizeof...(Chars)-N-1>(MetaString<Chars>{}...);
+}
+
+// Return the first N characters as a MetaString
+template<size_t N, char... Chars>
+constexpr auto First(MetaString<Chars...>) {
+	return _metastring::_First<N>(NullMetaString{}, MetaString<Chars...>{});
+}
+
+// Return the last N characters in a MetaString
+template<size_t N, char... Chars>
+constexpr auto Last(MetaString<Chars...>) {
+	return _metastring::_Last<N>(NullMetaString{}, MetaString<Chars...>{});
+}
+
+// Return the substring between indices [Start, End)
+template<size_t Start, size_t End, char... Chars>
+constexpr auto Substring(MetaString<Chars...>) {
+	static_assert(Start <= End, "Invalid start index");
+	static_assert(End <= sizeof...(Chars), "Invalid end index");
+	return First<End-Start>(Last<sizeof...(Chars)-Start>(MetaString<Chars...>{}));
+}
+
+// Reverse a MetaString
+template<char... Chars>
+constexpr auto Reverse(MetaString<Chars...>) {
+	return _metastring::_Reverse(MetaString<Chars...>{}, NullMetaString{});
+}
+
+// Removes leading whitespace and null characters
+template<char... Chars>
+constexpr auto LStrip(MetaString<Chars...>) {
+	return _metastring::_LStrip(MetaString<Chars...>{});
+}
+
+// Remove trailing whitespace and null characters
+template<char... Chars>
+constexpr auto RStrip(MetaString<Chars...>) {
+	return Reverse(LStrip(Reverse(MetaString<Chars...>{})));
+}
+
+// Remove leading and trailing whitespace and null characters
+template<char... Chars>
+constexpr auto Strip(MetaString<Chars...>) {
+	return RStrip(LStrip(MetaString<Chars...>{}));
+}
+
+// Split string on char X and return a list of substrings
+template<char X, char... Chars>
+constexpr auto Split(MetaString<Chars...>) {
+	return _metastring::_Split<X>(MetaString<Chars...>{}, NullMetaString{}, EmptyMetaList{});
+}
+
+// Join MetaStrings with separator MetaString
+template<char... Sep, char... Chars>
+constexpr auto Join(MetaString<Sep...>, MetaString<Chars...>) {
+	return MetaString<Chars...>{};
+}
+
+template<char... Sep, char... First, char... Second, typename... Rest>
+constexpr auto Join(MetaString<Sep...>, MetaString<First...>, MetaString<Second...>, Rest...) {
+	return Join(MetaString<Sep...>{}, MetaString<First...,Sep...,Second...>{}, Rest{}...);
+}
+
+template<char... Sep, typename... Items>
+constexpr auto Join(MetaString<Sep...>, MetaList<Items...>) {
+	if constexpr(0 == sizeof...(Items)) {
+		return NullMetaString{};
+	} else {
+		return Join(MetaString<Sep...>{}, Items{}...);
+	}
+}
+
+// Join MetaStrings with single char
+template<char Sep, typename... Strings>
+constexpr auto Join(Strings...) {
+	return Join(MetaString<Sep>{}, Strings{}...);
+}
+
+template<char Sep, typename... Items>
+constexpr auto Join(MetaList<Items...>) {
+	return Join(MetaString<Sep>{}, MetaList<Items...>{});
+}
+
+
+// Concatenate multiple MetaStrings
+template<char... First, typename... Rest>
+constexpr auto Concatenate(MetaString<First...>, Rest...) {
+	return Join(NullMetaString{}, MetaString<First...>{}, Rest{}...);
+}
+
+template<typename... Items>
+constexpr auto Concatenate(MetaList<Items...>) {
+	if constexpr(0 == sizeof...(Items)) {
+		return NullMetaString{};
+	} else {
+		return Concatenate(Items{}...);
+	}
+}
+
+// Comparison operators
+template<char... LHS, char... RHS>
+constexpr bool operator==(MetaString<LHS...>, MetaString<RHS...>) {
+	return false;
+}
+
+template<char... Chars>
+constexpr bool operator==(MetaString<Chars...>, MetaString<Chars...>) {
+	return true;
+}
+
+template<char... LHS, char... RHS>
+constexpr bool operator!=(MetaString<LHS...>, MetaString<RHS...>) {
+	return !(MetaString<LHS...>{} == MetaString<RHS...>{});
+}
+
+
+
+// Now we use to pre-processing hacks to generate long lists
+// of calls to GetCharOrNull. These tokens get concatenated and placed in the
+// template parameter list of a MetaString class declaration
+
+#define METASTRING_GET_CHAR_AT(_index, _literal) \
+	::estp::_metastring::GetCharOrNull<_index>(_literal)
+
+#define METASTRING_GET_16_CHARS_AT(_base, _literal) \
+    METASTRING_GET_CHAR_AT(_base+ 0, _literal), \
+    METASTRING_GET_CHAR_AT(_base+ 1, _literal), \
+    METASTRING_GET_CHAR_AT(_base+ 2, _literal), \
+    METASTRING_GET_CHAR_AT(_base+ 3, _literal), \
+    METASTRING_GET_CHAR_AT(_base+ 4, _literal), \
+    METASTRING_GET_CHAR_AT(_base+ 5, _literal), \
+    METASTRING_GET_CHAR_AT(_base+ 6, _literal), \
+    METASTRING_GET_CHAR_AT(_base+ 7, _literal), \
+    METASTRING_GET_CHAR_AT(_base+ 8, _literal), \
+    METASTRING_GET_CHAR_AT(_base+ 9, _literal), \
+    METASTRING_GET_CHAR_AT(_base+10, _literal), \
+    METASTRING_GET_CHAR_AT(_base+11, _literal), \
+    METASTRING_GET_CHAR_AT(_base+12, _literal), \
+    METASTRING_GET_CHAR_AT(_base+13, _literal), \
+    METASTRING_GET_CHAR_AT(_base+14, _literal), \
+    METASTRING_GET_CHAR_AT(_base+15, _literal)
+
+#define METASTRING_GET_64_CHARS_AT(_base, _literal) \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 0, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 1, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 2, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 3, _literal)
+
+#define METASTRING_GET_256_CHARS_AT(_base, _literal) \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 0, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 1, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 2, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 3, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 4, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 5, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 6, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 7, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 8, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16* 9, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16*10, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16*11, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16*12, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16*13, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16*14, _literal), \
+	METASTRING_GET_16_CHARS_AT(_base + 16*15, _literal)
+
+// Subtract one from sizeof(...) to account for null terminator
+#define MAKE_METASTRING_16(_literal) \
+	First<sizeof(_literal)-1>(::estp::_metastring::CheckMetaStringMaxLen<sizeof(_literal)-1 <= 16>(\
+		::estp::MetaString<METASTRING_GET_16_CHARS_AT(0,_literal)>{}))
+
+// Subtract one from sizeof(...) to account for null terminator
+#define MAKE_METASTRING_64(_literal) \
+	First<sizeof(_literal)-1>(::estp::_metastring::CheckMetaStringMaxLen<sizeof(_literal)-1 <= 64>(\
+		::estp::MetaString<METASTRING_GET_64_CHARS_AT(0,_literal)>{}))
+
+// Subtract one from sizeof(...) to account for null terminator
+#define MAKE_METASTRING_256(_literal) \
+	First<sizeof(_literal)-1>(::estp::_metastring::CheckMetaStringMaxLen<sizeof(_literal)-1 <= 256>(\
+		::estp::MetaString<METASTRING_GET_256_CHARS_AT(0,_literal)>{}))
+
+
+// This can *really* slow down compilation times, so use sparingly
+#ifdef ESTP_ENABLE_METASTRING_1024
+
+#define METASTRING_CHARS_AT_1024(_base, _literal) \
+	METASTRING_GET_256_CHARS_AT(_base + 256*0, _literal), \
+	METASTRING_GET_256_CHARS_AT(_base + 256*1, _literal), \
+	METASTRING_GET_256_CHARS_AT(_base + 256*2, _literal), \
+	METASTRING_GET_256_CHARS_AT(_base + 256*3, _literal)
+
+#define MAKE_METASTRING_1024(_literal) \
+	Strip(::estp::_metastring::CheckMetaStringMaxLen<sizeof(_literal)-1 <= 1024>(\
+		::estp::MetaString<METASTRING_GET_1024_CHARS_AT(0,_literal)>{}))
+
+#endif
+
+#ifndef METASTRING_DEFAULT_SIZE
+#define METASTRING_DEFAULT_SIZE 64
+#endif
+
+#if METASTRING_DEFAULT_SIZE <= 16
+#define MAKE_METASTRING(_literal) MAKE_METASTRING_16(_literal)
+#elif METASTRING_DEFAULT_SIZE <= 64
+#define MAKE_METASTRING(_literal) MAKE_METASTRING_64(_literal)
+#else
+#define MAKE_METASTRING(_literal) MAKE_METASTRING_256(_literal)
+#endif
+
+} // namespace estp
+
---
+++ lib/data/meta_string_test.cc
@@ -0,0 +1,215 @@
+
+#include "data/meta_string.h"
+
+// MetStrings are meta-types, so manipulation of them all occurs at compile
+// time. Thus, we don't use a standard run-time unit test framework
+// If this test file compiles, then all the tests passed!
+
+using namespace estp;
+
+// Some test fodder
+#define LETTERS "ABCDE"
+using Letters = MetaString<'A','B','C','D','E'>;
+
+#define NUMBERS "1234"
+using Numbers = MetaString<'1','2','3','4'>;
+
+#define ZIPPED "A1B2C3D4E"
+using Zipped = MetaString<'A','1','B','2','C','3','D','4','E'>;
+
+////////////////////////////////////////////////////////////
+// Comparison operators
+// We test these first because we rely on them extensively in later tests
+
+static_assert(Letters{} == Letters{}, "Test: Positive equality comparison");
+static_assert(!(Letters{} == Numbers{}), "Test: Negative equality comparison");
+
+static_assert(Letters{} != Numbers{}, "Test: Positive inequality comparison");
+static_assert(!(Numbers{} != Numbers{}), "Test: Negative inequality comparison");
+
+constexpr auto kEmptyString = MetaString<>{};
+static_assert(MetaString<>{} == kEmptyString, "Test: Equality of empty string");
+static_assert(Letters{} != kEmptyString, "Test: Inequality of empty string");
+static_assert(kEmptyString != Numbers{}, "Test: Inequality of empty string (reverse)");
+
+////////////////////////////////////////////////////////////
+// Construction
+
+constexpr auto kLetters = MAKE_METASTRING_16(LETTERS);
+static_assert(Letters{} == kLetters, "Test: MAKE_METASTRING(Letters)");
+
+constexpr auto kNumbers = MAKE_METASTRING_16(NUMBERS);
+static_assert(Numbers{} == kNumbers, "Test: MAKE_METASTRING(Numbers)");
+
+constexpr auto kZipped = MAKE_METASTRING_64(ZIPPED);
+static_assert(Zipped{} == kZipped, "Test: MAKE_METASTRING(Zipped)");
+
+static_assert(kEmptyString == MAKE_METASTRING_16(""), "Test: MAKE_METASTRING(\"\")");
+
+static_assert(MetaString<0x00,0x32,0x64,0x96,0xC8,0xFA>{} ==
+	MAKE_METASTRING_16("\x00\x32\x64\x96\xC8\xFA"),
+	"Test: MAKE_METASTRING('\x00\x32\x64\x96\xC8\xFA')");
+
+////////////////////////////////////////////////////////////
+// Element Access
+
+static_assert('A' == Get<0>(kLetters), "Test: Get<0>(Letters)");
+static_assert('B' == Get<1>(kLetters), "Test: Get<1>(Letters)");
+static_assert('C' == Get<2>(kLetters), "Test: Get<2>(Letters)");
+static_assert('D' == Get<3>(kLetters), "Test: Get<3>(Letters)");
+static_assert('E' == Get<4>(kLetters), "Test: Get<4>(Letters)");
+
+static_assert('E' == RGet<0>(kLetters), "Test: RGet<0>(Letters)");
+static_assert('D' == RGet<1>(kLetters), "Test: RGet<1>(Letters)");
+static_assert('C' == RGet<2>(kLetters), "Test: RGet<2>(Letters)");
+static_assert('B' == RGet<3>(kLetters), "Test: RGet<3>(Letters)");
+static_assert('A' == RGet<4>(kLetters), "Test: RGet<4>(Letters)");
+
+////////////////////////////////////////////////////////////
+// First N
+
+static_assert(kEmptyString == First<0>(kNumbers), "Test: First<0>(Numbers)");
+static_assert(MetaString<'1'>{} == First<1>(kNumbers), "Test: First<1>(Numbers)");
+static_assert(MetaString<'1','2'>{} == First<2>(kNumbers), "Test: First<2>(Numbers)");
+static_assert(MetaString<'1','2','3'>{} == First<3>(kNumbers), "Test: First<3>(Numbers)");
+static_assert(MetaString<'1','2','3','4'>{} == First<4>(kNumbers), "Test: First<4>(Numbers)");
+static_assert(MetaString<'1','2','3','4'>{} == First<5>(kNumbers), "Test: First<5>(Numbers)");
+
+////////////////////////////////////////////////////////////
+// Last N
+
+static_assert(kEmptyString == Last<0>(kNumbers), "Test: Last<0>(Numbers)");
+static_assert(MetaString<'4'>{} == Last<1>(kNumbers), "Test: Last<1>(Numbers)");
+static_assert(MetaString<'3','4'>{} == Last<2>(kNumbers), "Test: Last<2>(Numbers)");
+static_assert(MetaString<'2','3','4'>{} == Last<3>(kNumbers), "Test: Last<3>(Numbers)");
+static_assert(MetaString<'1','2','3','4'>{} == Last<4>(kNumbers), "Test: Last<4>(Numbers)");
+static_assert(MetaString<'1','2','3','4'>{} == Last<5>(kNumbers), "Test: Last<5>(Numbers)");
+
+////////////////////////////////////////////////////////////
+// Substring
+
+static_assert(kEmptyString == Substring<0,0>(kEmptyString), "Test: Substring<0,0>(kEmptyString)");
+static_assert(kEmptyString == Substring<0,0>(kZipped), "Test: Substring<0,0>(kZipped)");
+static_assert(kZipped == Substring<0,9>(kZipped), "Test: Substring<0,9>(kZipped)");
+static_assert(MAKE_METASTRING("A1B") == Substring<0,3>(kZipped), "Test: Substring<0,3>(kZipped)");
+static_assert(MAKE_METASTRING("C3D4E") == Substring<4,9>(kZipped), "Test: Substring<4,9>(kZipped)");
+static_assert(MAKE_METASTRING("B2C3D") == Substring<2,7>(kZipped), "Test: Substring<2,7>(kZipped)");
+
+////////////////////////////////////////////////////////////
+// Reverse
+
+static_assert(MetaString<>{} == Reverse(kEmptyString), "Test: Reverse(EmptyString)");
+static_assert(MetaString<'A'>{} == Reverse(MetaString<'A'>{}), "Test: Reverse(MetaString<A>)");
+static_assert(MetaString<'E','D','C','B','A'>{} == Reverse(kLetters), "Test: Reverse(Letters)");
+
+////////////////////////////////////////////////////////////
+// LStrip
+
+static_assert(kEmptyString == LStrip(kEmptyString), "Test: LStrip(kEmptyString)");
+static_assert(kEmptyString == LStrip(MetaString<' ','\t'>{}),
+	"Test: LStrip(MetaString<SPACE,TAB>)");
+
+static_assert(kNumbers == LStrip(MetaString<' ','\t',' ','\t','1','2','3','4'>{}),
+	"Test: LStrip(MetaString<SPACE,TAB,SPACE,TAB,Numbers>)");
+
+static_assert(MetaString<'1','2','3','4','\t'>{} ==
+	LStrip(MetaString<' ','\t',' ','\t','1','2','3','4','\t'>{}),
+	"Test: LStrip(MetaString<SPACE,TAB,SPACE,TAB,Numbers,TAB>)");
+
+////////////////////////////////////////////////////////////
+// RStrip
+
+static_assert(kEmptyString == RStrip(kEmptyString), "Test: RStrip(kEmptyString)");
+static_assert(kEmptyString == RStrip(MetaString<' ','\t','\t'>{}),
+	"Test: RStrip(MetaString<SPACE,TAB,TAB>)");
+
+static_assert(kNumbers == RStrip(MetaString<'1','2','3','4',' ','\t',' ','\t'>{}),
+	"Test: RStrip(MetaString<Numbers,SPACE,TAB,SPACE,TAB>)");
+
+
+static_assert(MetaString<'\t','1','2','3','4'>{} ==
+	RStrip(MetaString<'\t','1','2','3','4',' ','\t',' ','\t'>{}),
+	"Test: RStrip(MetaString<TAB,Numbers,SPACE,TAB,SPACE,TAB>)");
+
+////////////////////////////////////////////////////////////
+// Strip
+
+constexpr auto kPadded = MAKE_METASTRING("\t\t  CONTENT  \t\t");
+
+static_assert(kEmptyString == Strip(kEmptyString), "Test: Strip(kEmptyString)");
+static_assert(MAKE_METASTRING("CONTENT") == Strip(kPadded), "Test: Strip(Padded)");
+
+////////////////////////////////////////////////////////////
+// Split
+
+static_assert(MetaList<MetaString<>>{} == Split<'$'>(kEmptyString), "Test: Split<'$'>(kEmptyString)");
+static_assert(MetaList<Zipped>{} == Split<'$'>(kZipped), "Test: Split<'$'>(Zipped)");
+
+constexpr auto kCsv = MAKE_METASTRING("first,last,phone,email");
+constexpr auto kSplitCsv = MetaList<
+	decltype(MAKE_METASTRING("first")),
+	decltype(MAKE_METASTRING("last")),
+	decltype(MAKE_METASTRING("phone")),
+	decltype(MAKE_METASTRING("email"))>{};
+
+static_assert(kSplitCsv== Split<','>(kCsv), "Test: Split(CSV)");
+
+constexpr auto kAbsPath = MAKE_METASTRING("/absolute/path/for/document.pdf");
+constexpr auto kSplitAbsPath = MetaList<
+	NullMetaString,
+	decltype(MAKE_METASTRING("absolute")),
+	decltype(MAKE_METASTRING("path")),
+	decltype(MAKE_METASTRING("for")),
+	decltype(MAKE_METASTRING("document.pdf"))>{};
+
+static_assert(kSplitAbsPath== Split<'/'>(kAbsPath), "Test: Split(AbsPath)");
+
+constexpr auto kBoxes = MAKE_METASTRING("||Col 1||Col 2||Col 3||");
+constexpr auto kSplitBoxes = MetaList<
+	NullMetaString,
+	NullMetaString,
+	decltype(MAKE_METASTRING("Col 1")),
+	NullMetaString,
+	decltype(MAKE_METASTRING("Col 2")),
+	NullMetaString,
+	decltype(MAKE_METASTRING("Col 3")),
+	NullMetaString,
+	NullMetaString>{};
+
+static_assert(kSplitBoxes == Split<'|'>(kBoxes), "Test: Split(Boxes)");
+
+////////////////////////////////////////////////////////////
+// Join
+
+static_assert(kEmptyString == Join<'$'>(kEmptyString), "Test: Join<'$'>(kEmptyString)");
+static_assert(kEmptyString == Join<'$'>(EmptyMetaList{}), "Test: Join<'$'>(EmptyList)");
+
+static_assert(kCsv == Join<','>(kSplitCsv), "Test: Join<','>(SplitCsv)");
+static_assert(kCsv == Join<','>(MAKE_METASTRING("first"), MAKE_METASTRING("last"),
+	MAKE_METASTRING("phone"), MAKE_METASTRING("email")), "Test: Join<','>(CSV Parts)");
+
+static_assert(kBoxes == Join<'|'>(kSplitBoxes), "Test: Join<'|'>(kSplitBoxes)");
+
+static_assert(MAKE_METASTRING("red and blue and green") == Join(MAKE_METASTRING(" and "),
+	MAKE_METASTRING("red"), MAKE_METASTRING("blue"), MAKE_METASTRING("green")),
+	"Test: Join(' and ', 'red', 'blue', 'green')");
+
+////////////////////////////////////////////////////////////
+// Concatenate
+
+static_assert(kEmptyString == Concatenate(kEmptyString,kEmptyString,kEmptyString),
+	"Test: Concatenate(kEmptyString,kEmptyString,kEmptyString)");
+
+static_assert(MAKE_METASTRING("redbluegreen") == Concatenate(MAKE_METASTRING("red"),
+	MAKE_METASTRING("blue"), MAKE_METASTRING("green")),
+	"Test: Concatenate('red','blue','green')");
+
+static_assert(MAKE_METASTRING("Col 1Col 2Col 3") == Concatenate(kSplitBoxes),
+	"Test: Concatenate(kSplitBoxes)");
+
+////////////////////////////////////////////////////////////
+// Pièce De Résistance
+
+static_assert(MAKE_METASTRING("meta_string_test.cc") ==
+	RGet<0>(Split<'/'>(MAKE_METASTRING_256(__FILE__))),
+	"Test: Get basename of current source file");
