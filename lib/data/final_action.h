a737a4c61c005bc6c069c7126b7915a89ddbca69
David R. Palchak <david@palchak.net>
2018-08-16 16:29:42 -0700

    Add FinalAction class for scope-level clean-up
    * A FinalAction object stores a function pointer or lambda expression
    that will be executed when the object is destroyed.
    * FinalAction objects can be created directly or via a call to finally(...)
    * FinalActions can be dismissed after creation, which prevents the action
    from occurring when the object is destroyed

---
+++ lib/data/final_action.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "base/macros.h"
+
+#include <utility>
+
+namespace estp {
+
+template<typename F>
+class FinalAction {
+public:
+    explicit FinalAction(F action) noexcept : _action(std::move(action)) {}
+
+    FinalAction(FinalAction && other) noexcept
+    	: _action(std::move(other._action)), _pending(other._pending) {
+        other._pending = false;
+    }
+
+    ~FinalAction() noexcept {
+        if (_pending) {
+            _action();
+        }
+    }
+
+    constexpr void dismiss() noexcept {
+    	_pending = false;
+    }
+
+    // Disable move assignment (but not move construct)
+    FinalAction& operator=(FinalAction &&) = delete;
+
+private:
+    F _action;
+    bool _pending{true};
+
+    DISABLE_COPY(FinalAction)
+};
+
+template<typename F>
+inline FinalAction<F> finally(F const &action) noexcept {
+    return FinalAction<F>(action);
+}
+
+template<typename F>
+inline FinalAction<F> finally(F &&action) noexcept {
+    return FinalAction<F>(std::forward<F>(action));
+}
+
+
+} // namespace estp
---
+++ lib/data/final_action_test.cc
@@ -0,0 +1,77 @@
+#include "data/final_action.h"
+#include "catch2/catch.hpp"
+
+#include <iostream>
+#include <string>
+#include <utility>
+
+using namespace estp;
+
+static int obj{0};
+
+static void SetObjToFive() {
+	obj = 5;
+}
+
+TEST_CASE("FinalAction fires") {
+	obj = 0;
+
+    SECTION( "FinalAction calls lambda function" ) {
+    	REQUIRE(0 == obj);
+    	{
+    		auto act = FinalAction{[](){ obj = 1; }};
+    	}
+        REQUIRE(1 == obj);
+    }
+
+    SECTION( "finally() calls lambda function" ) {
+    	REQUIRE(0 == obj);
+    	{
+    		auto act = finally( [](){ obj = 1; } );
+    	}
+        REQUIRE(1 == obj);
+    }
+
+    SECTION( "FinalAction calls static function" ) {
+    	REQUIRE(0 == obj);
+    	{
+    		auto act = FinalAction{&SetObjToFive};
+    	}
+        REQUIRE(5 == obj);
+    }
+
+    SECTION( "finally() calls static function" ) {
+    	REQUIRE(0 == obj);
+    	{
+    		auto act = finally(&SetObjToFive);
+    	}
+        REQUIRE(5 == obj);
+    }
+}
+
+TEST_CASE("FinalAction can be dismissed") {
+	obj = 0;
+
+    SECTION( "FinalAction calls lambda function" ) {
+    	REQUIRE(0 == obj);
+    	{
+    		auto act = FinalAction{[](){ obj = 3; }};
+    		act.dismiss();
+    	}
+        REQUIRE(0 == obj);
+    }
+}
+
+TEST_CASE("FinalAction can be moved") {
+	obj = 0;
+
+    SECTION( "FinalAction move construction" ) {
+    	REQUIRE(0 == obj);
+    	{
+    		auto act = FinalAction{&SetObjToFive};
+    		auto act2 = std::move(act);
+    		act2.dismiss();
+    	}
+        REQUIRE(0 == obj);
+    }
+}
