c43db17210d9e0f2e8527cc76b4fb13845d9b9ba
David R. Palchak <david@palchak.net>
2018-08-16 16:34:13 -0700

    Add more self-explanatory casting operations
    * Adds up_cast<T>(), down_cast<T>(), narrow_cast<T>(),
    and range_cast<T>() as synonyms for static_cast<T>()
    * The appropriate cast should be used to convey the operation that the
    author is expecting to occur
    * Adds enum_cast() to cast an enumeration class value to the underlying_type
    for the enumeration class
    * Add narrow<T>() to perform narrowing cast with run-time checking for
    overflow and underflow

---
+++ lib/base/casting.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "base/macros.h"
+#include "base/types.h"
+
+#include <type_traits>
+#include <utility>
+
+namespace estp {
+
+namespace _casting {
+
+template<typename T, typename U>
+constexpr bool HaveDifferentSignedness() {
+	return std::is_signed<T>::value != std::is_signed<U>::value;
+}
+
+template<typename T>
+constexpr bool IsPositive(T t) {
+	return t >= T();
+}
+
+} // namespace _casting
+
+template<typename T, typename U>
+INLINE constexpr static decltype(auto) narrow_cast(U && u) noexcept {
+    return static_cast<T>(std::forward<U>(u));
+}
+
+template<typename T, typename U>
+INLINE constexpr static decltype(auto) range_cast(U && u) noexcept {
+    return static_cast<T>(std::forward<U>(u));
+}
+
+template<typename T, typename U>
+inline T narrow(U u) {
+    T t = narrow_cast<T>(u);
+    ASSERT(static_cast<U>(t) == u);
+    if(_casting::HaveDifferentSignedness<T,U>()) {
+    	ASSERT(_casting::IsPositive<T>(t) == _casting::IsPositive<U>(u));
+    }
+    return t;
+}
+
+template<typename T, typename U>
+INLINE constexpr static decltype(auto) down_cast(U && u) noexcept {
+	return static_cast<T>(std::forward<U>(u));
+}
+
+template<typename T, typename U>
+INLINE constexpr static decltype(auto) up_cast(U && u) noexcept {
+	return static_cast<T>(std::forward<U>(u));
+}
+
+template<typename E>
+INLINE constexpr static decltype(auto) enum_cast(E val) noexcept {
+  return static_cast<typename ::std::underlying_type<E>::type>(val);
+}
+
+} // namespace estp
---
+++ lib/base/casting_test.cc
@@ -0,0 +1,37 @@
+#include "base/assert.h"
+#include "base/casting.h"
+#include "base/types.h"
+#include "catch2/catch.hpp"
+
+#include <iostream>
+#include <string>
+#include <utility>
+
+using namespace estp;
+
+static int assert_count{0};
+
+static void HandleAssertion(AssertionContext const &) {
+    ++assert_count;
+}
+
+TEST_CASE("narrow() asserts") {
+    assert_count = 0;
+    SetAssertionHandler(&HandleAssertion);
+
+    SECTION( "narrow() asserts on value truncation" ){
+    REQUIRE(0 == assert_count);
+        unsigned a = 256;
+        uint8_t b = narrow<uint8_t>(a);
+        REQUIRE(1 == assert_count);
+        REQUIRE(unsigned(b) != a);
+    }
+
+    SECTION( "narrow() asserts on overflow" ){
+        REQUIRE(0 == assert_count);
+        uint8_t a = 128;
+        int8_t b = narrow<int8_t>(a);
+        REQUIRE(1 == assert_count);
+        REQUIRE(int(b) != int(a));
+    }
+}
