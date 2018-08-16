0282c243645ccca11cf1d28c66ad3da93291b693
David R. Palchak <david@palchak.net>
2018-08-16 16:37:30 -0700

    Add at() universal range-checked accessor
    * Adds family of free functions named at() for accessing values in
    random access containers
    * Analagous to std::data() and std::size()
    * Idea borrows from the C++ GSL
    * Overloads should be defined for appropriate containers

---
+++ lib/base/at.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "base/assert.h"
+#include "base/casting.h"
+#include "base/types.h"
+
+#include <array>
+
+// Basic implementation of the at() function as described in the C++ GSL
+
+namespace estp {
+
+template<typename T, Index N>
+constexpr decltype(auto) at(T (&arr)[N], Index index) {
+	EXPECTS(0 <= index && index < N);
+	return arr[index];
+}
+
+template<typename T, std::size_t N>
+constexpr decltype(auto) at(std::array<T, N> &arr, Index index) {
+	EXPECTS(0 <= index && index < narrow_cast<Index>(N));
+	return arr[index];
+}
+
+template<typename Container>
+constexpr decltype(auto) at(Container &c, Index index) {
+	EXPECTS(0 <= index && index < narrow_cast<Index>(c.size()));
+	return c[index];
+}
+
+template<typename T>
+constexpr decltype(auto) at(std::initializer_list<T> c, Index index) {
+	EXPECTS(0 < = index && index < narrow_cast<Index>(c.size()));
+	return *(c.begin() + index);
+}
+
+}
