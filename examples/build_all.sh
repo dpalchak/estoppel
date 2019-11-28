a21d5d53fc53a2094c7361e4ac0ff373a62b1c1c
David R. Palchak <david@palchak.net>
2019-11-27 22:41:08 -0800

    Revise examples/build_all.sh to use Ninja instead of Makefiles

--- examples/build_all.sh
+++ examples/build_all.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
-
 while IFS= read -r -d '' CMAKE_LIST_FILE; do
   SRC_DIR=$(dirname "${CMAKE_LIST_FILE}")
   BUILD_DIR="./BUILD/${SRC_DIR}"
+  rm -rf "${BUILD_DIR}"
   mkdir -p "${BUILD_DIR}"
-  cmake -B "${BUILD_DIR}" "${SRC_DIR}"
-  make -C "${BUILD_DIR}"
+  cmake -B "${BUILD_DIR}" -G "Ninja" "${SRC_DIR}"
+  ninja -C "${BUILD_DIR}"
 done < <(find . -type f -name 'CMakeLists.txt' -print0)
